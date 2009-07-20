ad74c99ca197063e9f4f78234e87acf187023213
patrick (Patrick Brown) <patrick.brown@utoronto.ca>
2009-07-20 12:48:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/deffbinary@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/DEFFbinary/R/DEFFbinary.R
@@ -0,0 +1,32 @@
+DEFFbinary = function(
+    logitMean, sigma, 
+    NperGroup=NULL,Ngroups=NULL,  Nsim = 200000) {
+
+  # simulate some normals to find the moments of the logit-normal  
+  theSim = rnorm(Nsim, logitMean, sigma)
+  # transform to logit-normals
+  theSim = exp(theSim) / (1 + exp(theSim))
+  # compute the moments
+  moment1 = mean(theSim)
+  moment1sq = moment1^2
+  moment2 = mean(theSim^2)
+  
+ # compute the ICC 
+ result = c(ICC = (moment2 - moment1sq) / (moment1 - moment1sq) )
+     
+ # if NperGroup is provided, compute the design effect    
+ if(!is.null(NperGroup))           
+ 	result["DEFF"] = 1+(NperGroup-1)*result["ICC"]
+
+
+# if Ngroups is specified, compute the standard error
+if(!is.null(Ngroups)) {
+	totalSize = NperGroup*Ngroups
+	result["SE"] = 1/sqrt( (totalSize/result["DEFF"]) * moment1*(1-moment1))	
+}
+
+# save the moments as attributes  
+ attributes(result)$moments = c(first=moment1, second=moment2)
+
+result
+}
