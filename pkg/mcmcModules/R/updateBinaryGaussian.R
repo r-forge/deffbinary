82376731b4c7ea0c2a48dc6a57974183859ecdec
patrick (Patrick Brown) <patrick.brown@utoronto.ca>
2010-03-16 16:33:36 +0000

    added mcmcModules
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/deffbinary@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mcmcModules/R/updateBinaryGaussian.R
@@ -0,0 +1,9 @@
+updateBinaryGaussian <- function(y, X=NULL, coef, offsetY, meanCoef, precisionCoef, 
+		niter=1, sdProposal=1, link=c("logit","cloglog"), ...) {
+	
+# use bayesglm in the arm package	
+	
+	
+	
+}
+
