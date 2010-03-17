6caa7a4a595548e2a698f3d91129bbb1abfd1bc5
patrick (Patrick Brown) <patrick.brown@utoronto.ca>
2010-03-17 17:50:12 +0000

    refining the updater
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/deffbinary@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mcmcModules/R/updateBinaryGaussian.R
+++ pkg/mcmcModules/R/updateBinaryGaussian.R
@@ -1,40 +1,67 @@
 updateBinaryGaussian <- function(y, X=NULL, coef, offsetY, meanCoef, precisionCoef, 
 		niter=1, sdProposal=1, link=c("logit","cloglog"), ...) {
-	
-# simulate proposal	
+
+link<-link[1]
+if(link=="logit") {
+	linkFun = function(qq) qq/ (1+qq)
+} else if(link=="cloglog"){
+	linkFun = function(qq) 1-exp(-qq)
+} else {
+	warning("link not found: ",link)
+}	
+
 if(length(coef)==1)	{ # univariate normal
-	proposedCoef <- rnorm(1,mean=coef, sd=sdProposal)	
+	# simulate from the proposal distribution
+	simProp <- function() rnorm(1,mean=coef, sd=sdProposal)
+	# function to calculate the log ratio of the 
+	# priors of new and proposed coefficients
+	priorDiff <- function() {
+		coefDiff = proposedCoef - coef
+		-0.5*(coefDiff*coefDiff*precisionCoef)
+	}
 } else {# multivariate
 	if(!is.matrix(sdProposal)){ 
 		# sdProposal is a vector of standard deviations rather than a variance matrix
 		sdProposal = diag(sdProposal, length(coef))
 	}
-	proposedCoef = as.vector(rmvnorm(1,mean=coef, sigma=sdProposal))
+	if(!is.matrix(precisionCoef)){
+		precisionCoef = diag(precisionCoef, length(coef))
+	}
+	simProp <- function() as.vector(rmvnorm(1,mean=coef, sigma=sdProposal))
+	priorDiff <- function() {
+		coefDiff = proposedCoef - coef
+		-0.5*(coefDiff %*% precisionCoef %*% coefDiff)
+	}
 }
 
-# calculate expenontials of (means on link scale)
-expMeanLinkOld = exp(offsetY + as.matrix(X) %*% coef)
-expMeanLinkNew = exp(offsetY + as.matrix(X) %*% proposedCoef)
+acceptRatio<-0
 
-link<-link[1]
-if(link=="logit") {
-	probsOld = expMeanLinkOld/ (1+expMeanLinkOld)
-	probsNew = expMeanLinkNew/ (1+expMeanLinkNew)
-} else if(link=="cloglog"){
-	probsOld = 1-exp(-expMeanLinkOld)
-	probsNew = 1-exp(-expMeanLinkNew)
-} else {
-	warning("link not found: ",link)
-}
+for(Diter in 1:niter){	
+	
+	# simulate proposal	
+	proposedCoef <- simProp()
+	
+	# calculate old and new probabilities
+	probsOld <- linkFun(exp(offsetY + as.matrix(X) %*% coef) )
+	probsNew <- linkFun(exp(offsetY + as.matrix(X) %*% proposedCoef) )
+
 	# calculate old and new likelihoods
 	onemy = 1-y
-#	logLikOld = sum(y*probsOld + onemy*(1-probsOld))  
-#	logLikNew = sum(y*probsNew + onemy*(1-probsNew))
-#	ratioSafe <- exp(logLikNew - logLikOld)
 	probsDiff = probsNew - probsOld
-	ratio <- exp(sum(y*probsDiff - onemy*probsDiff))
+	ratio <- exp(sum(y*probsDiff - onemy*probsDiff) + priorDiff())
 
 	accept = runif(1)<ratio
-	if(accept) result <- 
+	
+	if(accept) {
+		coef <- proposedCoef 
+		acceptRatio <- acceptRatio + accept
+	}
+} # end iteration loop
+			
+	attributes(coef)$mcmc <- c(acceptRatio=acceptRatio/niter,
+				niter=niter)
+	attributes(coef)$sdProposal = sdProposal	
+	
+	coef
 }
 
