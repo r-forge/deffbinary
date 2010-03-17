b841170e77eb5cadca0f1e3c80de01f6deb8f137
patrick (Patrick Brown) <patrick.brown@utoronto.ca>
2010-03-17 15:43:01 +0000

    simple mh sampler added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/deffbinary@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mcmcModules/R/updateBinaryGaussian.R
+++ pkg/mcmcModules/R/updateBinaryGaussian.R
@@ -1,9 +1,40 @@
 updateBinaryGaussian <- function(y, X=NULL, coef, offsetY, meanCoef, precisionCoef, 
 		niter=1, sdProposal=1, link=c("logit","cloglog"), ...) {
 	
-# use bayesglm in the arm package	
-	
-	
-	
+# simulate proposal	
+if(length(coef)==1)	{ # univariate normal
+	proposedCoef <- rnorm(1,mean=coef, sd=sdProposal)	
+} else {# multivariate
+	if(!is.matrix(sdProposal)){ 
+		# sdProposal is a vector of standard deviations rather than a variance matrix
+		sdProposal = diag(sdProposal, length(coef))
+	}
+	proposedCoef = as.vector(rmvnorm(1,mean=coef, sigma=sdProposal))
+}
+
+# calculate expenontials of (means on link scale)
+expMeanLinkOld = exp(offsetY + as.matrix(X) %*% coef)
+expMeanLinkNew = exp(offsetY + as.matrix(X) %*% proposedCoef)
+
+link<-link[1]
+if(link=="logit") {
+	probsOld = expMeanLinkOld/ (1+expMeanLinkOld)
+	probsNew = expMeanLinkNew/ (1+expMeanLinkNew)
+} else if(link=="cloglog"){
+	probsOld = 1-exp(-expMeanLinkOld)
+	probsNew = 1-exp(-expMeanLinkNew)
+} else {
+	warning("link not found: ",link)
+}
+	# calculate old and new likelihoods
+	onemy = 1-y
+#	logLikOld = sum(y*probsOld + onemy*(1-probsOld))  
+#	logLikNew = sum(y*probsNew + onemy*(1-probsNew))
+#	ratioSafe <- exp(logLikNew - logLikOld)
+	probsDiff = probsNew - probsOld
+	ratio <- exp(sum(y*probsDiff - onemy*probsDiff))
+
+	accept = runif(1)<ratio
+	if(accept) result <- 
 }
 
